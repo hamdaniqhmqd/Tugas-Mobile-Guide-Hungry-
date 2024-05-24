a9b915ddac6279c1fab7512a97d6547c6da152be
ahmad-hamdani <ahmadhamdaniiii2020@gmail.com>
2024-05-24 23:01:55 +0700

    tambahan tampilan home dan menampilkan data makanan

---
+++ app/src/main/java/com/tugasuas/tugasuas/database/Makanan.kt
@@ -0,0 +1,12 @@
+package com.tugasuas.tugasuas.database
+
+data class Makanan(
+    val gambar: Int,
+    val judul:String,
+    val jenis:String,
+    val waktu:String,
+    val alat:String,
+    val bahan:String,
+    val langkah:String,
+    val deskripsi:String,
+)
