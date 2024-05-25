b06eaf5d1b83ff6bd1e8570c51b56f4c60b79e8e
ahmad-hamdani <ahmadhamdaniiii2020@gmail.com>
2024-05-25 13:45:04 +0700

    penyesuaian

---
+++ app/src/main/java/com/tugasuas/tugasuas/katalog/katalog.kt
@@ -0,0 +1,21 @@
+package com.tugasuas.tugasuas.katalog
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ahmad.tugasuas.R
+
+class katalog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_katalog)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
