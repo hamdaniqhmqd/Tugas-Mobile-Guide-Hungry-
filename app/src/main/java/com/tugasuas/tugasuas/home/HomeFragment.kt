9ff4c631901e43dd9b78b797b6ea14cfef375401
ahmad-hamdani <ahmadhamdaniiii2020@gmail.com>
2024-06-02 10:30:20 +0700

    perbaikan dan memberikan comment

--- app/src/main/java/com/tugasuas/tugasuas/home/HomeFragment.kt
+++ app/src/main/java/com/tugasuas/tugasuas/home/HomeFragment.kt
@@ -9,18 +9,21 @@ import android.view.ViewGroup
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.ahmad.tugasuas.R
 import com.ahmad.tugasuas.databinding.FragmentHomeBinding
 import com.tugasuas.tugasuas.database.DataMakanan
-import com.tugasuas.tugasuas.database.Makanan
 import com.tugasuas.tugasuas.detail_data.detail_data
 import java.util.Locale
 
 class HomeFragment : Fragment() {
+    // Vvariabel untuk menyimpan binding yang dapat null
     private var _binding: FragmentHomeBinding? = null
+    // Ggetter untuk binding yang tidak boleh bernilai null,
+    // menggunakan _binding dengan not-null assertion
     private val binding get() = _binding!!
+    // membuat variabel HomeAdapter untuk memanggil homeAdapter
     private lateinit var HomeAdapter: homeAdapter
+
+    // untuk mengatur dan menampilkan tampilan ui dari home fragment
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,35 +32,44 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    // ketika tampilan UI dari fragment dihancurkan
     override fun onDestroyView() {
         super.onDestroyView()
+        // binding menjadi null untuk menghindari memory leaks
         _binding = null
     }
 
+    // metode yang dipanggil setelah tampilan UI telah dibuat
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // ketika SearchView di klik maka akan membuka pencarian
         binding.inputPencarian.setOnClickListener {
             binding.inputPencarian.isIconified = false
         }
-
+        // listener untuk perubahan teks pada SearchView
         binding.inputPencarian.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-
+            // fun akan berjalan ketika teks pencarian disubmit dan mengembalikan nilai true
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return true
             }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                filterList(newText.toString())
+            // ketika teks pencarian berubah kode didalamnya akan di eksekusi
+            override fun onQueryTextChange(kataKunci: String?): Boolean {
+                // Memanggil metode untuk memfilter data berdasarkan newText yang dimasukkan
+                filterList(kataKunci.toString())
                 return true
             }
-
         })
+        // untuk mengambil data makanan() dari file Makanan.kt
         val makanan = DataMakanan()
+        // untuk mengidentifikasi home adapter dengan data makanan
         HomeAdapter = homeAdapter(makanan)
 
+        // untuk mengatur layout manager dan adapter untuk RecyclerView
         binding.ViewDataMakanan.layoutManager = GridLayoutManager(context, 2)
         binding.ViewDataMakanan.adapter = HomeAdapter
 
+        // saat salah satu item pada RecyclerView
+        // di klik maka akan mengeksekusi kode didalamnya
         HomeAdapter.onItemClick = {
             val intent = Intent(context, detail_data::class.java)
             intent.putExtra("Makanan", it)
@@ -65,16 +77,21 @@ class HomeFragment : Fragment() {
         }
     }
 
+    // untuk memfilter data berdasarkan query pencarian
     private fun filterList(query: String) {
+        // mengambil data makanan dari file makanan.kt
         val data = DataMakanan()
+        // mengidentifikasi data sesuai query
         val filteredList = data.filter {
             it.judul.lowercase(Locale.ROOT).contains(query.lowercase(Locale.ROOT)) ||
             it.jenis.name.lowercase(Locale.ROOT).contains(query.lowercase(Locale.ROOT))
         }
+        // jika data hasil filter kosong atau data yang di cari tidak ada,
+        // tampilkan pesan, jika tidak kosong tampilkan data sesuai pencarian
         if (filteredList.isEmpty()) {
             Toast.makeText(context, "Data tidak ditemukan", Toast.LENGTH_SHORT).show()
         } else {
-            (binding.ViewDataMakanan.adapter as? homeAdapter)?.setFilteredList(filteredList) // Assuming adapter has a function named setFilteredList
+            (binding.ViewDataMakanan.adapter as? homeAdapter)?.setFilteredList(filteredList)
         }
     }
 }
--- app/src/main/java/com/tugasuas/tugasuas/katalog/katalogAdapter.kt
+++ app/src/main/java/com/tugasuas/tugasuas/katalog/katalogAdapter.kt
@@ -12,19 +12,22 @@ import com.ahmad.tugasuas.R
 
 class katalogAdapter(private var dataMakanan: List<Makanan>)
     : RecyclerView.Adapter<katalogAdapter.MakananViewHolder>(){
-
+    // variabel untuk mengambil nilai makanan
+    // dan tidak mengembalikan nilai apapun yang bisa berniali null
     var onItemClick: ((Makanan) -> Unit)? = null
-
+    // untuk mengidentifikasi data apa saja yang akan di tampilkan di recyclerView
     class MakananViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
         val ivMhs: ImageView = row.findViewById(R.id.ivGambar)
         val tvJudul: TextView = row.findViewById(R.id.tvJudul)
         val tvJenis: TextView = row.findViewById(R.id.tvJenis)
         val tvWaktu: TextView = row.findViewById(R.id.tvWaktu)
     }
-
+    // untuk membuat ViewHolder baru yang berisi tampilan item
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MakananViewHolder {
         val layout =
-            LayoutInflater.from(parent.context).inflate(R.layout.data_makanan, parent, false)
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.data_makanan, parent, false)
+        // Mengembalikan instance dari MakananViewHolder yang berisi view yang baru dibuat
         return MakananViewHolder(layout)
     }
 
@@ -41,6 +44,8 @@ class katalogAdapter(private var dataMakanan: List<Makanan>)
         holder.tvJenis.text = makanan.jenis.toString()
         holder.tvWaktu.text = makanan.waktu
 
+        // jika item dari recyclerView di click makan
+        // akan mengeksekusi kode didalamnya
         holder.itemView.setOnClickListener {
             onItemClick?.invoke(makanan)
         }
